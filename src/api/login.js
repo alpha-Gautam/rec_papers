3d2a103aa13574f90493aa3b061589cf0b1d5ad8
Gautam Maurya <gautammaurya1299@gmail.com>
2025-01-03 02:17:38 +0530

    ..

---
+++ src/api/login.js
@@ -0,0 +1,19 @@
+// import { server } from "../serve"
+
+// const url = ''
+
+// export const signUp = (data: any) => {
+//    return server.getSignUp(url + '/signup', data)
+// }
+
+// export const login = (data: any) => {
+//    return server.getSignUp(url + '/login', data)
+// }
+
+// export const isLogin = () => {
+//    return server.post(url + 'status', {})
+// }
+
+// export const serverSignout = () => {
+//    return server.post(url + '/signout', {})
+// }
