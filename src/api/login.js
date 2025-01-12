31caceb06ca31499a2a9ed6feacc7e8767660106
Gautam Maurya <gautammaurya1299@gmail.com>
2025-01-12 18:59:32 +0530

    baseurl change

--- src/api/login.js
+++ src/api/login.js
@@ -1,13 +1,18 @@
 import { server } from "../serve";
 import axios from "axios";
 
-const baseUrl = "http://127.0.0.1:8000/";
+let baseURL = "";
+if (window.location.href.includes("localhost")) {
+  baseURL = "http://127.0.0.1:8000";
+} else if (window.location.href.includes("onrender.com")) {
+  baseURL = "https://recpapers-backend.onrender.com/";
+}
 
 const url = "/api";
 
 export const userLoginapi = async (data) => {
   //   return server.get(url + "/login/", data);
-  const response = await axios.post(baseUrl + "api/login/", data);
+  const response = await axios.post(baseURL + "api/login/", data);
   //   const response = await server.post("api/login/", data);
 
   return response;
@@ -15,7 +20,7 @@ export const userLoginapi = async (data) => {
 
 export const userRagister = async (data) => {
   //   return server.get(url + "/login/", data);
-  const response = await axios.post(baseUrl + "api/ragister/", data);
+  const response = await axios.post(baseURL + "api/ragister/", data);
   //   const response = await server.post("api/login/", data);
 
   return response;
