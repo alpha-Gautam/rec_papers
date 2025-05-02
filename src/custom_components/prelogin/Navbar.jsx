77ae792d39c6c6551081de81712d4efd33d7a41d
Gautam Maurya <gautammaurya1299@gmail.com>
2025-05-02 12:57:16 +0530

    change logo

--- src/custom_components/prelogin/Navbar.jsx
+++ src/custom_components/prelogin/Navbar.jsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import logo from "./Logo.png";
+import logo1 from "./logo1.png";
 
 const Navbar = () => {
   const navigate = useNavigate();
@@ -33,7 +34,7 @@ const Navbar = () => {
       <div className="container mx-auto px-2 py-3 flex justify-between items-center">
         {/* Logo Section */}
         <div className="flex items-center">
-          <img src={logo} alt="Website Logo" className="h-10 mr-2" />
+          <img src={logo1} alt="Website Logo" className="h-10 mr-2" />
           <span className="text-2xl font-bold">
             <Link
               to="/"
